fa58a3dd38814d323095150269555113953f65e4
Balaji Gaikwad <gaikwadbalaji9@gmail.com>
2023-05-05 16:41:38 +0530

    assignemnet 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const heading2 = React.createElement("h1", null, "Hello world using React");
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 root2.render(heading2);
